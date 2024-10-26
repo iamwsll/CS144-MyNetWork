6d1619966611ee04296bb6136750dc60982d9a6d
iamwsll <1481121704@qq.com>
2024-10-27 00:22:21 +0800

    check3_code

--- src/tcp_sender.cc
+++ src/tcp_sender.cc
@@ -1,42 +1,149 @@
 #include "tcp_sender.hh"
 #include "tcp_config.hh"
-
+#include "cmath"
+#include "iostream"
 using namespace std;
 
 uint64_t TCPSender::sequence_numbers_in_flight() const
 {
-  // Your code here.
-  return {};
+  return _lastSendSq - _lastAckSq;
 }
 
 uint64_t TCPSender::consecutive_retransmissions() const
 {
-  // Your code here.
-  return {};
+  return _consecutiveRetransmissionsNum;
 }
 
 void TCPSender::push( const TransmitFunction& transmit )
 {
-  // Your code here.
-  (void)transmit;
+  TCPSenderMessage message = make_empty_message();
+  if(isFINmessage())setFINMessage(message);
+  else readAndPushMsg(message);
+  sendFromQueue(transmit);
 }
 
 TCPSenderMessage TCPSender::make_empty_message() const
 {
-  // Your code here.
-  return {};
+  return 
+  {
+    .seqno = Wrap32::wrap(_lastSendSq,isn_),
+    .RST = input_.has_error()
+  };
 }
 
 void TCPSender::receive( const TCPReceiverMessage& msg )
 {
-  // Your code here.
-  (void)msg;
+  if(msg.RST)input_.set_error();
+  if(msg.ackno)
+  {
+    uint64_t ackValue = msg.ackno.value().unwrap(isn_ , _lastAckSq);
+    if(_lastSendSq< ackValue)return;
+    if(_lastAckSq < ackValue)
+    {
+      if(_timerInside.isRunning()&&sequence_numbers_in_flight())_timerInside.resetTimer();
+      _timerInside.resetRTO(); 
+      _lastAckSq = ackValue;
+      _consecutiveRetransmissionsNum = 0;
+      _zeroFlag = false; 
+    }
+  }
+  _windowSize = msg.window_size;
 }
 
 void TCPSender::tick( uint64_t ms_since_last_tick, const TransmitFunction& transmit )
 {
-  // Your code here.
-  (void)ms_since_last_tick;
-  (void)transmit;
-  (void)initial_RTO_ms_;
+  if(_timerInside.isRunning())_timerInside.addTimer(ms_since_last_tick);
+  if(_timerInside.outOfTime())
+  {
+    while(!_delayQueue.empty())
+    {
+      TCPSenderMessage reSendMsg = _delayQueue.front();
+      if(reSendMsg.seqno.unwrap(isn_,_lastSendSq) + reSendMsg.sequence_length()>_lastAckSq)
+      {
+        transmit(reSendMsg);
+        _consecutiveRetransmissionsNum++;
+        _timerInside.resetTimer();
+        if(_windowSize!=0) _timerInside.doubleRTO();
+        break;
+      }
+      else _delayQueue.pop();
+    }
+     
+  }
+}
+//private:
+bool TCPSender::isFINmessage()
+{
+  return !_isFIN && input_.reader().is_finished() && _windowSize > sequence_numbers_in_flight();
+}
+
+void TCPSender::setFINMessage(TCPSenderMessage& message)
+{
+    message.SYN = !_isSYN;
+    message.FIN = _isFIN = true;
+    pushIntoSendQueue(message);
+    //std::cerr<<"oh , first if has been use"<<std::endl;
+}
+void TCPSender::pushIntoSendQueue(TCPSenderMessage& message)
+{
+  _lastSendSq+=message.sequence_length();
+  _sendQueue.push(message);
+}
+void TCPSender::readAndPushMsg(TCPSenderMessage& message)
+{
+    uint64_t readNum = std::min({_windowSize - sequence_numbers_in_flight(),input_.reader().bytes_buffered(),_windowSize});
+    if(readNum == 0)
+    {
+       if(!_isSYN)
+      {
+        message.SYN =_isSYN = true;
+        pushIntoSendQueue(message);
+      }
+      else if(_windowSize == 0 && !_zeroFlag)
+      { 
+        if(_lastAckSq == _lastSendSq && input_.reader().is_finished())message.FIN = true;
+        else read(input_.reader(), 1 ,message.payload); 
+        _zeroFlag = true;
+        pushIntoSendQueue(message);        
+      }
+    }
+
+      while(readNum > 0)
+      {
+        uint64_t onceReadLength = std::min(readNum,TCPConfig::MAX_PAYLOAD_SIZE);
+        message = make_empty_message();
+        read(input_.reader(),onceReadLength,message.payload);
+        message.SYN = !_isSYN;
+        message.FIN = input_.reader().is_finished();
+        checkAndFixFIN(message);
+        if(!_isSYN)
+        {
+          _isSYN = true;
+        }
+        pushIntoSendQueue(message);
+        readNum -= onceReadLength;
+      }
+}
+void TCPSender::sendFromQueue(const TransmitFunction& transmit)
+{
+   while (!_sendQueue.empty())
+    {
+      // if(_sendQueue.front().sequence_length() + sequence_numbers_in_flight() > _windowSize)
+      //   {
+      //     _sendQueue.front().FIN = false;
+      //     _isFIN = false;
+      //   }
+      if(!_timerInside.isRunning())_timerInside.startRunning();
+      if(_sendQueue.front().FIN) _isFIN = true;
+      transmit(_sendQueue.front());
+      _delayQueue.push(_sendQueue.front());
+      _sendQueue.pop();
+    }
+}
+void TCPSender::checkAndFixFIN(TCPSenderMessage& message)
+{
+  if(message.sequence_length() + sequence_numbers_in_flight() > _windowSize )
+  {
+    message.FIN = false;
+  }
 }
--- src/tcp_sender.hh
+++ src/tcp_sender.hh
@@ -11,12 +11,53 @@
 #include <optional>
 #include <queue>
 
+class RetransmissionTimer
+{
+public:
+  RetransmissionTimer(uint64_t initial_RTO_ms):_initial_RTO_ms_(initial_RTO_ms),_RTO(initial_RTO_ms){}
+  void startRunning()
+  {
+    _timerIsRunning = true;
+    _timer = 0;
+  }
+  void resetRTO()
+  {
+    _RTO = _initial_RTO_ms_;
+  }
+  bool isRunning()
+  {
+    return _timerIsRunning;
+  }
+  void resetTimer()
+  {
+    _timer = 0;
+  }
+  void addTimer(uint64_t ms)
+  {
+    _timer+=ms;
+  }
+  bool outOfTime()
+  {
+    return _timer >= _RTO;
+  }
+  void doubleRTO()
+  {
+    _RTO *=2;
+  }
+private:
+  uint64_t _timer{};
+  bool _timerIsRunning{};
+  uint64_t _initial_RTO_ms_;
+  uint64_t _RTO;
+};
+
+
 class TCPSender
 {
 public:
   /* Construct TCP sender with given default Retransmission Timeout and possible ISN */
   TCPSender( ByteStream&& input, Wrap32 isn, uint64_t initial_RTO_ms )
-    : input_( std::move( input ) ), isn_( isn ), initial_RTO_ms_( initial_RTO_ms )
+    : input_( std::move( input ) ), isn_( isn ), _timerInside(initial_RTO_ms)
   {}
 
   /* Generate an empty TCPSenderMessage */
@@ -47,5 +88,22 @@ private:
   // Variables initialized in constructor
   ByteStream input_;
   Wrap32 isn_;
-  uint64_t initial_RTO_ms_;
+  uint64_t _lastAckSq{};
+  uint64_t _lastSendSq{};
+  uint64_t _consecutiveRetransmissionsNum{};
+  uint64_t _windowSize{1};
+  std::queue<TCPSenderMessage> _sendQueue{};
+  std::queue<TCPSenderMessage> _delayQueue{};
+  bool _isSYN{};
+  bool _isFIN{};
+  bool _zeroFlag{};
+
+  RetransmissionTimer _timerInside;
+
+  bool isFINmessage();
+  void setFINMessage(TCPSenderMessage& message);
+  void pushIntoSendQueue(TCPSenderMessage& message);
+  void readAndPushMsg(TCPSenderMessage& message);
+  void sendFromQueue(const TransmitFunction& transmit);
+  void checkAndFixFIN(TCPSenderMessage& message);
 };
